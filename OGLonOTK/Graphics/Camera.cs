c5eed8f85d4946fb2bb2333df450e372a63266d0
Kolkovich <go1go23456789@gmail.com>
2026-03-30 04:40:21 +0300

    Предыдущий бестолковый merge, вызванный изменением сообщения к фиксу... попытка создать НОВЫЙ merge при изменении сообщения к последнему. Ладно, хер бы с ним. В этот раз он соизволил записать новые вещи?

---
+++ OGLonOTK/Graphics/Camera.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Mathematics;
+
+namespace OGLonOTK.Graphics
+{
+    public class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Front { get; set; } = -Vector3.UnitZ;
+        public Vector3 Up { get; set; } = Vector3.UnitY;
+
+        public Camera(Vector3 position)
+        {
+            Position = position;
+        }
+
+        public Matrix4 GetViewMatrix()
+        {
+            return Matrix4.LookAt(Position, Position + Front, Up);
+        }
+    }
+}
